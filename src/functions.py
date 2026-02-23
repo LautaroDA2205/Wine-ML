0f6a95234ce917b1570a929e4bf0b7bc321e4eb9
Lautaro <lautaro.csilvestri@gmail.com>
2026-02-23 15:19:29 +0100

    Wine Project Formalization

---
+++ src/functions.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def load_wine_data(path):
+    """
+    Carga el dataset de vinos y asigna nombres a las columnas.
+    """
+    columns = [
+        "class", "alcohol", "malic_acid", "ash", "alcalinity_of_ash",
+        "magnesium", "total_phenols", "flavanoids",
+        "nonflavanoid_phenols", "proanthocyanins",
+        "color_intensity", "hue",
+        "od280_od315", "proline"
+    ]
+
+    df = pd.read_csv(path, header=None)
+    df.columns = columns
+
+    # Ajustamos clases a 0,1,2
+    df["class"] = df["class"] - 1
+
+    return df
+
+
+def class_summary(df):
+    """
+    Devuelve estadísticas descriptivas agrupadas por clase.
+    """
+    return df.groupby("class").mean()
